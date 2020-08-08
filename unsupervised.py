c07ff8470855ae1f067fc625ef4aee71075a0d41
Mateus Bizzo <bizzo.mateus@gmail.com>
2020-08-08 16:21:25 -0300

    aula 2

--- unsupervised.py
+++ unsupervised.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 movies = pd.read_csv("movies.csv")
+# get categorical var genres column and convert into dummy/indicator vars
 genres = movies.genres.str.get_dummies()
 
 movies_data = pd.concat([movies, genres], axis=1)
@@ -9,4 +11,7 @@ movies_data = pd.concat([movies, genres], axis=1)
 scaler = StandardScaler()
 scaled_genres = scaler.fit_transform(genres)
 
-print(scaled_genres.shape)
+model = KMeans(n_clusters=5)
+model.fit(scaled_genres)
+
+print(f"Clusters {model.labels_}")
